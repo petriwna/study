e885d1e0a5a05b7bd50fce2a77fc85b3abc2fb84
petriwna <petriwna43@gmail.com>
2025-05-22 21:10:09 +0300

    Updates

--- main.77bf161fabe39e77d23f.hot-update.js
+++
@@ -1,2 +0,0 @@
-"use strict";self.webpackHotUpdatevebinar("main",{},(function(c){c.h=()=>"583ccd0df20a03240dcf"}));
-//# sourceMappingURL=main.77bf161fabe39e77d23f.hot-update.js.map
---
+++ main.d740aa72f9c3449b35e8.hot-update.js
@@ -0,0 +1,2 @@
+"use strict";self.webpackHotUpdatevebinar("main",{},(function(e){e.h=()=>"ed3c4b145f666062671f"}));
+//# sourceMappingURL=main.d740aa72f9c3449b35e8.hot-update.js.map
